710d2c8e789089b6be34539bfeb2c831c76fd901
gEc4AwAlgroA <gEc4AwAlgroA@users.noreply.github.com>
2017-01-23 17:53:11 +0000

    poly and calc working

--- week4/calculator/src/main/scala/calculator/Calculator.scala
+++ week4/calculator/src/main/scala/calculator/Calculator.scala
@@ -11,11 +11,19 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+        namedExpressions mapValues(v => Signal(eval(v(), namedExpressions)))
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => eval(getReferenceExpr(name, references), references - name)
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+      case _ => math.random
+    }
   }
 
   /** Get the Expr for a referenced variables.
--- week4/calculator/src/main/scala/calculator/Polynomial.scala
+++ week4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,17 @@
 package calculator
+import scala.math
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal(b() * b() - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal(delta() match {
+      case x if x >= 0 => Set((-b() + math.sqrt(x)) / (2 * a()), (-b() - math.sqrt(x)) / (2 * a()))
+      case _ => Set()
+    })
   }
 }
